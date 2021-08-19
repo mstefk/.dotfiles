0f11d8d6f0dfb81b3a7fc9e0311b716f46799cb7
mstefk <68973887+mstefk@users.noreply.github.com>
2021-08-19 20:20:44 +0200

    Add files via upload

---
+++ dotfiles_setup.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/bash
+
+chmod +x ~/.dotfiles/.scripts/*
+chmod +x ~/.dotfiles/.dwm/autostart.sh
+
+ln -s ~/.dotfiles/.bashrc ~/.bashrc
+ln -s ~/.dotfiles/.bash_profile ~/.bash_profile
+ln -s ~/.dotfiles/.xinitrc ~/.xinitrc
+ln -s ~/.dotfiles/.Xresources ~/.Xresources
+ln -s ~/.dotfiles/.scripts/ ~/.scripts
+ln -s ~/.dotfiles/.dwm/ ~/.dwm
+ln -s ~/.dotfiles/.wallpaper ~/.wallpaper
